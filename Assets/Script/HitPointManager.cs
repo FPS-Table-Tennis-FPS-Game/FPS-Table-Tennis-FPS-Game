17a21b72526e6134c2c709775dfa71cca9568c53
jinheehan <lsd0017@naver.com>
2024-06-26 22:28:17 +0900

    Add Ball Dispenser and Fix etc..
    2024-06-26
    - Add Ball Dispenser
    - Divide Scene ( SampleScene_Yunsang, SampleScene_JinHee )

--- Assets/Script/HitPointManager.cs
+++ Assets/Script/HitPointManager.cs
@@ -25,7 +25,17 @@ public class HitPointManager : MonoBehaviour
             
             //플레이어의 방향 으로 공 보내기 : 카메라 각도 방향으로 공 보내기
             float force = uIcontroller.GetCurrentGauge() * 1000;
+
+            //공을 어느정도 띄울 경우
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * 400);
+
+            //공을 미는(치는) 힘
             other.GetComponent<Rigidbody>().AddForce(transform.parent.forward * force);
+
+            //공을 미는 힘이 약하면 공이 많이 띄워짐
+            //공을 미는 힘이 강하면 공이 적게 띄워짐
+            //지속적인 테스트를 통해 밸런스 조정 예정
+
             if(ballController.servState == true)
             {
                 Destroy(ballController.generatedFrame);
