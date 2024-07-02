6ebe8c1fb425e4485dfd1ce3bd2027ca61962541
jinheehan <lsd0017@naver.com>
2024-07-02 22:26:26 +0900

    Add BackSwing And Fix Swing Power
    2024-07-02
    - Add BackSwing, BackSwing Motion

--- Assets/Script/UserMovController.cs
+++ Assets/Script/UserMovController.cs
@@ -11,51 +11,64 @@ public class UserMovController : MonoBehaviour
     public Animator PlayerAnim;
     public BoxCollider StrokeRange;
     public TrailRenderer RacketEffect;
-    private UIcontroller uIcontroller;
+
+    private HitPointManager userHitPoint;
+    private Gaugecontroller uIcontroller;
+    private AimingController aimingController;
 
     private BallController ballController;
     public bool ishit = false;
-    private bool isCharging = false;
 
 
     private Vector3 stopState = new Vector3(0f, 0f, 0f);
     void Start()
     {
+        userHitPoint = GetComponentInChildren<HitPointManager>();
         PlayerRigidBody = GetComponent<Rigidbody>();
         PlayerRigidBody.position = movement;
         GameObject canvas = GameObject.Find("Canvas");
-        uIcontroller = canvas.GetComponent<UIcontroller>();
-
+        uIcontroller = canvas.GetComponent<Gaugecontroller>();
+        aimingController = canvas.GetComponent<AimingController>();
         ballController = GameObject.Find("Ball Director").GetComponent<BallController>();
     }
 
     void Update()
     {
         Run();
+
         //Mouse button press > charge > mouse button up > hit  > Gauge reset
         if (Input.GetMouseButton(0))
         {
             //When Mouse clicked turn on swing effect
-
-
-
             uIcontroller.AddGauge(0.01f);
-
-
         }
-
 
         else if (Input.GetMouseButtonUp(0))
         {
+            userHitPoint.swingType = 0;
+            RacketEffect.enabled = true;
             StartCoroutine(WaitStrokeEffect());
             StartCoroutine(WaitStroke());
             PlayerAnim.SetTrigger("Swing");
             StartCoroutine(WaitReset());
-
-
-
         }
 
+        //Mouse button press > charge > mouse button up > hit  > Gauge reset
+        if (Input.GetMouseButton(1))
+        {
+            //When Mouse clicked turn on swing effect
+            uIcontroller.AddGauge(0.01f);
+        }
+
+        else if (Input.GetMouseButtonUp(1))
+        {
+            userHitPoint.swingType = 1;
+            RacketEffect.enabled = true;
+            StartCoroutine(WaitStrokeEffect());
+            StartCoroutine(WaitStroke());
+            PlayerAnim.SetTrigger("BackSwing");
+            StartCoroutine(WaitReset());
+        }
     }
 
     IEnumerator WaitStrokeEffect()
@@ -68,9 +81,9 @@ public class UserMovController : MonoBehaviour
     }
     IEnumerator WaitStroke()
     {
-        yield return new WaitForSeconds(0.04f);
-        StrokeRange.enabled = true;
         yield return new WaitForSeconds(0.02f);
+        StrokeRange.enabled = true;
+        yield return new WaitForSeconds(0.08f);
         StrokeRange.enabled = false;
     }
 
@@ -90,10 +103,12 @@ public class UserMovController : MonoBehaviour
             if (inputMoveXZ != stopState)
             {
                 PlayerAnim.SetBool("Running", true);
+                aimingController.Run(true);
             }
             else
             {
                 PlayerAnim.SetBool("Running", false);
+                aimingController.Run(false);
             }
 
             float inputMoveXZMgnitude = inputMoveXZ.sqrMagnitude;
