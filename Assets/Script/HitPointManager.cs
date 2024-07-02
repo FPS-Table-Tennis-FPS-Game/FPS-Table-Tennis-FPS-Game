d1982210c03ed650df4943a46b932f41d8b6baaf
YunsangLeeCV <148689090+YunsangLeeCV@users.noreply.github.com>
2024-07-02 10:03:14 +0900

    조준점
    조준점

--- Assets/Script/HitPointManager.cs
+++ Assets/Script/HitPointManager.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class HitPointManager : MonoBehaviour
 {
-    private UIcontroller uIcontroller;
+    private Gaugecontroller uIcontroller;
     private BallController ballController;
+    private AimingController aimingController;
+    public int[] inaccuracy;
     // Start is called before the first frame update
     void Start()
     {
         GameObject canvas = GameObject.Find("Canvas");
-        uIcontroller = canvas.GetComponent<UIcontroller>();
+        uIcontroller = canvas.GetComponent<Gaugecontroller>();
+        aimingController = canvas.GetComponent<AimingController>();
+
         GameObject ballDirector = GameObject.Find("Ball Director");
         ballController = ballDirector.GetComponent<BallController>();
-        
+
+        // 벡터를 위한 x,y,z 값
+        inaccuracy = new int[3]{ 0,0,0};
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,21 +28,30 @@ public class HitPointManager : MonoBehaviour
         if (other.tag.Equals("Ball"))
         {
             other.GetComponent<Rigidbody>().isKinematic = false;
-            
+
+            //공을 쳤기에 에이밍 포인트 사라짐        
+            aimingController.Hitball();
+
+            //부정확 방향
+            Vector3 distubrance = new Vector3((float)inaccuracy[0], (float)inaccuracy[1], (float)inaccuracy[2]);
+
             //플레이어의 방향 으로 공 보내기 : 카메라 각도 방향으로 공 보내기
             float force = uIcontroller.GetCurrentGauge() * 1000;
 
             //공을 어느정도 띄울 경우
             other.GetComponent<Rigidbody>().AddForce(Vector3.up * 400);
 
-            //공을 미는(치는) 힘
-            other.GetComponent<Rigidbody>().AddForce(transform.parent.forward * force);
+            //부정확성 + 공을 미는(치는) 힘  
+            other.GetComponent<Rigidbody>().AddForce(distubrance + transform.parent.forward * force);
+       
 
             //공을 미는 힘이 약하면 공이 많이 띄워짐
             //공을 미는 힘이 강하면 공이 적게 띄워짐
             //지속적인 테스트를 통해 밸런스 조정 예정
 
-            if(ballController.servState == true)
+            
+
+            if (ballController.servState == true)
             {
                 Destroy(ballController.generatedFrame);
                 ballController.servState = false;
--- Assets/Script/UserMovController.cs
+++ Assets/Script/UserMovController.cs
@@ -11,7 +11,9 @@ public class UserMovController : MonoBehaviour
     public Animator PlayerAnim;
     public BoxCollider StrokeRange;
     public TrailRenderer RacketEffect;
-    private UIcontroller uIcontroller;
+    
+    private Gaugecontroller uIcontroller;
+    private AimingController aimingController;
 
     private BallController ballController;
     public bool ishit = false;
@@ -24,36 +26,30 @@ public class UserMovController : MonoBehaviour
         PlayerRigidBody = GetComponent<Rigidbody>();
         PlayerRigidBody.position = movement;
         GameObject canvas = GameObject.Find("Canvas");
-        uIcontroller = canvas.GetComponent<UIcontroller>();
-
+        uIcontroller = canvas.GetComponent<Gaugecontroller>();
+        aimingController = canvas.GetComponent<AimingController>();
         ballController = GameObject.Find("Ball Director").GetComponent<BallController>();
     }
 
     void Update()
     {
         Run();
+        
+        //게이지 충전
         //Mouse button press > charge > mouse button up > hit  > Gauge reset
         if (Input.GetMouseButton(0))
         {
             //When Mouse clicked turn on swing effect
-
-
-
             uIcontroller.AddGauge(0.01f);
-
-
         }
 
-
+        //게이지 방출
         else if (Input.GetMouseButtonUp(0))
         {
             StartCoroutine(WaitStrokeEffect());
             StartCoroutine(WaitStroke());
             PlayerAnim.SetTrigger("Swing");
             StartCoroutine(WaitReset());
-
-
-
         }
 
     }
@@ -90,10 +86,12 @@ public class UserMovController : MonoBehaviour
             if (inputMoveXZ != stopState)
             {
                 PlayerAnim.SetBool("Running", true);
+                aimingController.Run(true);
             }
             else
             {
                 PlayerAnim.SetBool("Running", false);
+                aimingController.Run(false);
             }
 
             float inputMoveXZMgnitude = inputMoveXZ.sqrMagnitude;
