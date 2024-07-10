d2e312c94fdfa02078f2fa0cf44fc5507643d4b0
JinHee <lsd0017@naver.com>
2024-07-10 10:59:07 +0900

    Fix MultiPlay  UI

--- Assets/Script/MultiPlay/MultiPlayerMovement.cs
+++ Assets/Script/MultiPlay/MultiPlayerMovement.cs
@@ -18,16 +18,19 @@ public class MultiPlayerMovement : NetworkBehaviour
     public TrailRenderer RacketEffect;
 
     public MultiHitPoint userHitPoint;
-    //private Gaugecontroller uIcontroller;
-    //private AimingController aimingController;
+    private MultiGuageController guageController;
+    private MultiAmingController aimingController;
 
     //private BallController ballController;
     public bool ishit = false;
     public bool isSwing = false;
 
+    public bool isLCharge = false;
+    public bool isRCharge = false;
+
     private Vector3 stopState = new Vector3(0f, 0f, 0f);
 
-    [Networked] public NetworkButtons ButtonsPrevious { get; set; }
+    [Networked] public bool EffectEnabled { get; set; }
 
     // Start is called before the first frame update
     public override void Spawned()
@@ -38,6 +41,11 @@ public class MultiPlayerMovement : NetworkBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             sight = Camera.main;
             sight.GetComponent<FirstPersonCamera>().Target = gameObject.transform.GetChild(1).transform;
+
+            GameObject canvas = GameObject.Find("Canvas");
+            guageController = canvas.GetComponent<MultiGuageController>();
+            aimingController = canvas.GetComponent<MultiAmingController>();
+            EffectEnabled = false;
         }
     }
 
@@ -48,40 +56,72 @@ public class MultiPlayerMovement : NetworkBehaviour
             return;
         }
 
-        //Mouse button press > charge > mouse button up > hit  > Gauge reset
-        if (Input.GetAxis("Fire1") == 1 && !isSwing)
+        if(!EffectEnabled)
         {
-            isSwing = true;
-            userHitPoint.swingType = 0;
+            RacketEffect.enabled = false;
+        } else
+        {
             RacketEffect.enabled = true;
-            StartCoroutine(WaitStrokeEffect());
-            StartCoroutine(WaitStroke());
-            PlayerAnim.SetTrigger("Swing");
-            //StartCoroutine(WaitReset());
         }
 
         //Mouse button press > charge > mouse button up > hit  > Gauge reset
-        if (Input.GetAxis("Fire2") == 1 && !isSwing)
+        if(Input.GetAxis("Fire2") != 1)
         {
-            isSwing = true;
-            userHitPoint.swingType = 1;
-            RacketEffect.enabled = true;
-            StartCoroutine(WaitStrokeEffect());
-            StartCoroutine(WaitStroke());
-            PlayerAnim.SetTrigger("BackSwing");
-           // StartCoroutine(WaitReset());
+            if (Input.GetAxis("Fire1") == 1 && !isLCharge)
+            {
+                isLCharge = true;
+            }
+            else if (Input.GetAxis("Fire1") == 1 && isLCharge)
+            {
+                guageController.AddGauge(0.02f);
+            }
+            else if (Input.GetAxis("Fire1") == 0 && isLCharge && !isSwing)
+            {
+                isSwing = true;
+                userHitPoint.swingType = 0;
+                EffectEnabled = true;
+                StartCoroutine(WaitStrokeEffect());
+                StartCoroutine(WaitStroke());
+                PlayerAnim.SetTrigger("Swing");
+                StartCoroutine(WaitReset());
+            }
+
+        }
+
+        if(Input.GetAxis("Fire1") != 1)
+        {
+            //Mouse button press > charge > mouse button up > hit  > Gauge reset
+            if (Input.GetAxis("Fire2") == 1 && !isRCharge)
+            {
+                isRCharge = true;
+            }
+            else if (Input.GetAxis("Fire2") == 1 && isRCharge)
+            {
+                guageController.AddGauge(0.02f);
+            }
+            else if (Input.GetAxis("Fire2") == 0 && isRCharge && !isSwing)
+            {
+                isSwing = true;
+                userHitPoint.swingType = 1;
+                EffectEnabled = true;
+                StartCoroutine(WaitStrokeEffect());
+                StartCoroutine(WaitStroke());
+                PlayerAnim.SetTrigger("BackSwing");
+                StartCoroutine(WaitReset());
+            }
         }
         Turn();
 
         Vector3 inputMoveXZ = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
         if (inputMoveXZ != stopState)
         {
             PlayerAnim.SetBool("Running", true);
+            aimingController.Run(true);
         }
         else
         {
             PlayerAnim.SetBool("Running", false);
+            aimingController.Run(false);
         }
 
 
@@ -105,10 +145,11 @@ public class MultiPlayerMovement : NetworkBehaviour
 
     IEnumerator WaitStrokeEffect()
     {
-        if (RacketEffect.enabled == true)
+        if (EffectEnabled == true)
         {
             yield return new WaitForSeconds(0.2f);
-            RacketEffect.enabled = false;
+            // RacketEffect.enabled = false;
+            EffectEnabled = false;
         }
     }
     IEnumerator WaitStroke()
@@ -119,12 +160,14 @@ public class MultiPlayerMovement : NetworkBehaviour
         StrokeRange.enabled = false;
         yield return new WaitForSeconds(0.02f);
         isSwing = false;
+        isLCharge = false;
+        isRCharge = false;
     }
 
     IEnumerator WaitReset()
     {
         yield return new WaitForSeconds(0.3f);
-       // uIcontroller.ResetGauge();
+        guageController.ResetGauge();
     }
 
     void Turn()
